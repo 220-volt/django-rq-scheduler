a244cde2efc2ab58a9a471468fef15d419d7d81c
Dima Veselov <d.a.veselov@yandex.ru>
2018-06-14 13:23:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 
 setup(
-    name='readwise-django-rq-scheduler',
+    name='django-rq-scheduler',
     version=get_version(HERE),
     description='A database backed job scheduler for Django RQ',
     long_description=long_description,
